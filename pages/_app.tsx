62c95cfc5dcae1ad7e282fb90c02061203090ef1
Daniel <Da_919@hotmail.com>
2023-04-19 20:23:27 -0500

    se implementa el componente analytics de vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { appWithTranslation } from 'next-i18next'
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { appWithTranslation } from "next-i18next";
+import { Analytics } from "@vercel/analytics/react";
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+};
 
 export default appWithTranslation(App);
