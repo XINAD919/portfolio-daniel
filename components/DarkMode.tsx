c790321b797689d38d6ad56fdfcb6db41de15670
Daniel <da_919@hotmail.com>
2023-07-13 17:59:08 -0500

    se estila el Darkmode button

--- components/DarkMode.tsx
+++ components/DarkMode.tsx
@@ -3,12 +3,16 @@ import { ThemeContext } from "../context/ThemeContext";
 const DarkMode = () => {
   const {theme, toggleTheme}=useContext(ThemeContext)
   return (
-    <div className='dark-mode-toggle'>
-      <label className='switch'>
+    <div className='flex flex-row-reverse switch-button'>
+      <label className='switch'htmlFor="switch-button">
         <input
           type='checkbox'
+          id='switch-button'
+          name='switch-button'
+          placeholder="switch"
           checked={theme === "Dark" }
           onChange={() => toggleTheme()}
+          readOnly
         />
         <span className='slider round' />
       </label>
