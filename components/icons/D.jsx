f5b965b62fc7ba5a29fea1fa0d0ba1b620669353
Daniel <da_919@hotmail.com>
2023-07-13 18:16:11 -0500

    se cambia de tsx a jsx por error de tipos

--- components/icons/D.jsx
+++ components/icons/D.jsx
@@ -5,11 +5,11 @@ const D = () => {
   const theme = useTheme()
   return (
     <svg
-      width='38'
-      height='38'
-      viewBox='0 0 38 38'
-      fill='none'
-      xmlns='http://www.w3.org/2000/svg'
+    width='38'
+    height='38'
+    viewBox='0 0 38 38'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
       stroke={`${theme.icon}`}
     >
       <circle cx='19' cy='19' r='18.5'/>
