586ff701fc35e615e29464f5751e1d326102d496
Daniel <da_919@hotmail.com>
2023-07-27 19:12:28 -0500

    scrollRestoration: true

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const { i18n } = require("./next-i18next.config");
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  experimental: {
+    scrollRestoration: true,
+  },
 };
 
 module.exports = nextConfig;
